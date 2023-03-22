47b8b13c7f768156877ff95358bd6c16fca6eaf0
Mikhail Gorshkov <mikhailgorshkovdeveloper@gmail.com>
2023-03-22 21:53:11 +0400

    create rest controller(Get, Put)

--- src/main/java/ru/shop/shopservice/ShopServiceApplication.java
+++ src/main/java/ru/shop/shopservice/ShopServiceApplication.java
@@ -3,33 +3,12 @@ package ru.shop.shopservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import ru.shop.shopservice.dto.Product;
-import ru.shop.shopservice.service.ProductService;
-
-import java.util.List;
 
 @SpringBootApplication
 public class ShopServiceApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext run = SpringApplication.run(ShopServiceApplication.class, args);
-
-        ProductService productService = run.getBean(ProductService.class);
-
-
-        Product product = new Product();
-        product.setId(2L);
-        product.setName("Book about Java");
-
-        Product product1 = new Product();
-        product.setId(1L);
-        product.setName("T-Shirt");
-
-        productService.save(product);
-
-        List<Product> products = productService.findByName("T-Shirt");
-
-        System.out.println(products);
 
     }
 
--- src/main/java/ru/shop/shopservice/controller/ProductController.java
+++ src/main/java/ru/shop/shopservice/controller/ProductController.java
@@ -1,12 +1,11 @@
 package ru.shop.shopservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.shop.shopservice.dto.Product;
 import ru.shop.shopservice.service.ProductService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/product")
@@ -16,9 +15,19 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
+    @PostMapping()
+    public Product save(@RequestBody Product product) {
+        return productService.save(product);
+    }
+
     @GetMapping("/{id}")
     public Product getProduct(@PathVariable Long id) {
         return productService.get(id);
+    }
+
+    @GetMapping("/search/findByName")
+    public List<Product> findByName(@RequestParam String name) {
+        return productService.findByName(name);
     }
 
 }
